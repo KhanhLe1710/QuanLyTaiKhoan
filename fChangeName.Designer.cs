ad9f743f822b245d321d8cb42ae58e6d6d116948
Le Hoang Khanh <lhkhanh1710@gmail.com>
2020-06-10 21:42:54 +0700

    fChangeName.Designer.cs

---
+++ fChangeName.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Quan_ly_tai_khoan
+{
+    partial class fchangname
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtpassword1 = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtnewname = new System.Windows.Forms.TextBox();
+            this.txtoldname = new System.Windows.Forms.TextBox();
+            this.btupdate = new System.Windows.Forms.Button();
+            this.lbthongbao4 = new System.Windows.Forms.Label();
+            this.lbthongbao3 = new System.Windows.Forms.Label();
+            this.lbthongbao2 = new System.Windows.Forms.Label();
+            this.lbthongbao1 = new System.Windows.Forms.Label();
+            this.btcancel = new System.Windows.Forms.Button();
+            this.lbpass2 = new System.Windows.Forms.Label();
+            this.lbpass1 = new System.Windows.Forms.Label();
+            this.lbnewname = new System.Windows.Forms.Label();
+            this.lboldname = new System.Windows.Forms.Label();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtpassword1);
+            this.panel1.Controls.Add(this.txtpassword);
+            this.panel1.Controls.Add(this.txtnewname);
+            this.panel1.Controls.Add(this.txtoldname);
+            this.panel1.Controls.Add(this.btupdate);
+            this.panel1.Controls.Add(this.lbthongbao4);
+            this.panel1.Controls.Add(this.lbthongbao3);
+            this.panel1.Controls.Add(this.lbthongbao2);
+            this.panel1.Controls.Add(this.lbthongbao1);
+            this.panel1.Controls.Add(this.btcancel);
+            this.panel1.Controls.Add(this.lbpass2);
+            this.panel1.Controls.Add(this.lbpass1);
+            this.panel1.Controls.Add(this.lbnewname);
+            this.panel1.Controls.Add(this.lboldname);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(570, 372);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtpassword1
+            // 
+            this.txtpassword1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword1.Location = new System.Drawing.Point(242, 240);
+            this.txtpassword1.Name = "txtpassword1";
+            this.txtpassword1.PasswordChar = '*';
+            this.txtpassword1.Size = new System.Drawing.Size(268, 30);
+            this.txtpassword1.TabIndex = 19;
+            this.txtpassword1.TextChanged += new System.EventHandler(this.txtpassword1_TextChanged);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.Location = new System.Drawing.Point(242, 173);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.Size = new System.Drawing.Size(268, 30);
+            this.txtpassword.TabIndex = 18;
+            this.txtpassword.TextChanged += new System.EventHandler(this.txtpassword_TextChanged);
+            // 
+            // txtnewname
+            // 
+            this.txtnewname.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnewname.Location = new System.Drawing.Point(242, 98);
+            this.txtnewname.Name = "txtnewname";
+            this.txtnewname.Size = new System.Drawing.Size(268, 30);
+            this.txtnewname.TabIndex = 17;
+            this.txtnewname.TextChanged += new System.EventHandler(this.txtnewname_TextChanged);
+            // 
+            // txtoldname
+            // 
+            this.txtoldname.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtoldname.Location = new System.Drawing.Point(242, 41);
+            this.txtoldname.Name = "txtoldname";
+            this.txtoldname.Size = new System.Drawing.Size(268, 30);
+            this.txtoldname.TabIndex = 16;
+            this.txtoldname.TextChanged += new System.EventHandler(this.txtoldname_TextChanged);
+            // 
+            // btupdate
+            // 
+            this.btupdate.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btupdate.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btupdate.Location = new System.Drawing.Point(105, 308);
+            this.btupdate.Name = "btupdate";
+            this.btupdate.Size = new System.Drawing.Size(111, 41);
+            this.btupdate.TabIndex = 15;
+            this.btupdate.Text = "Cập Nhật";
+            this.btupdate.UseVisualStyleBackColor = true;
+            this.btupdate.Click += new System.EventHandler(this.btupdate_Click);
+            // 
+            // lbthongbao4
+            // 
+            this.lbthongbao4.AutoSize = true;
+            this.lbthongbao4.Location = new System.Drawing.Point(216, 276);
+            this.lbthongbao4.Name = "lbthongbao4";
+            this.lbthongbao4.Size = new System.Drawing.Size(0, 17);
+            this.lbthongbao4.TabIndex = 13;
+            // 
+            // lbthongbao3
+            // 
+            this.lbthongbao3.AutoSize = true;
+            this.lbthongbao3.Location = new System.Drawing.Point(216, 214);
+            this.lbthongbao3.Name = "lbthongbao3";
+            this.lbthongbao3.Size = new System.Drawing.Size(0, 17);
+            this.lbthongbao3.TabIndex = 12;
+            // 
+            // lbthongbao2
+            // 
+            this.lbthongbao2.AutoSize = true;
+            this.lbthongbao2.Location = new System.Drawing.Point(216, 139);
+            this.lbthongbao2.Name = "lbthongbao2";
+            this.lbthongbao2.Size = new System.Drawing.Size(0, 17);
+            this.lbthongbao2.TabIndex = 11;
+            // 
+            // lbthongbao1
+            // 
+            this.lbthongbao1.AutoSize = true;
+            this.lbthongbao1.Location = new System.Drawing.Point(216, 74);
+            this.lbthongbao1.Name = "lbthongbao1";
+            this.lbthongbao1.Size = new System.Drawing.Size(0, 17);
+            this.lbthongbao1.TabIndex = 10;
+            // 
+            // btcancel
+            // 
+            this.btcancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btcancel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btcancel.Location = new System.Drawing.Point(338, 308);
+            this.btcancel.Name = "btcancel";
+            this.btcancel.Size = new System.Drawing.Size(111, 41);
+            this.btcancel.TabIndex = 9;
+            this.btcancel.Text = "Huỷ";
+            this.btcancel.UseVisualStyleBackColor = true;
+            // 
+            // lbpass2
+            // 
+            this.lbpass2.AutoSize = true;
+            this.lbpass2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbpass2.Location = new System.Drawing.Point(6, 243);
+            this.lbpass2.Name = "lbpass2";
+            this.lbpass2.Size = new System.Drawing.Size(207, 22);
+            this.lbpass2.TabIndex = 6;
+            this.lbpass2.Text = "Xác Nhận Lại Mật Khẩu:";
+            // 
+            // lbpass1
+            // 
+            this.lbpass1.AutoSize = true;
+            this.lbpass1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbpass1.Location = new System.Drawing.Point(6, 181);
+            this.lbpass1.Name = "lbpass1";
+            this.lbpass1.Size = new System.Drawing.Size(93, 22);
+            this.lbpass1.TabIndex = 4;
+            this.lbpass1.Text = "Mật Khẩu:";
+            // 
+            // lbnewname
+            // 
+            this.lbnewname.AutoSize = true;
+            this.lbnewname.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbnewname.Location = new System.Drawing.Point(6, 106);
+            this.lbnewname.Name = "lbnewname";
+            this.lbnewname.Size = new System.Drawing.Size(173, 22);
+            this.lbnewname.TabIndex = 2;
+            this.lbnewname.Text = "Tên Tài Khoản Mới:";
+            // 
+            // lboldname
+            // 
+            this.lboldname.AutoSize = true;
+            this.lboldname.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lboldname.Location = new System.Drawing.Point(6, 44);
+            this.lboldname.Name = "lboldname";
+            this.lboldname.Size = new System.Drawing.Size(161, 22);
+            this.lboldname.TabIndex = 0;
+            this.lboldname.Text = "Tên Tài Khoản Cũ:";
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            this.errorProvider.RightToLeftChanged += new System.EventHandler(this.txtoldname_TextChanged);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            this.errorProvider1.RightToLeftChanged += new System.EventHandler(this.txtnewname_TextChanged);
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            this.errorProvider2.RightToLeftChanged += new System.EventHandler(this.txtpassword_TextChanged);
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            this.errorProvider3.RightToLeftChanged += new System.EventHandler(this.txtpassword1_TextChanged);
+            // 
+            // fchangname
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btcancel;
+            this.ClientSize = new System.Drawing.Size(591, 399);
+            this.Controls.Add(this.panel1);
+            this.Name = "fchangname";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đổi Tên Tài Khoản";
+            this.Load += new System.EventHandler(this.fchangname_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbnewname;
+        private System.Windows.Forms.Label lboldname;
+        private System.Windows.Forms.Button btcancel;
+        private System.Windows.Forms.Label lbpass2;
+        private System.Windows.Forms.Label lbpass1;
+        private System.Windows.Forms.Label lbthongbao4;
+        private System.Windows.Forms.Label lbthongbao3;
+        private System.Windows.Forms.Label lbthongbao2;
+        private System.Windows.Forms.Label lbthongbao1;
+        private System.Windows.Forms.Button btupdate;
+        private System.Windows.Forms.TextBox txtoldname;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.TextBox txtpassword1;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtnewname;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+    }
+}
