285b823998e6ecf23f70cc7839f36ce12d7df6a2
Le Hoang Khanh <lhkhanh1710@gmail.com>
2020-06-10 21:48:31 +0700

    GlobalSuppressions.cs

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Quan_ly_tai_khoan.Form1.btlogin_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Quan_ly_tai_khoan.Form1.bthuy_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Quan_ly_tai_khoan.fchangname.btupdate_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Quan_ly_tai_khoan.fTableManager.đổiTênTàiKhoảnToolStripMenuItem_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Quan_ly_tai_khoan.fTableManager.đổiMậtKhẩuSốToolStripMenuItem_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Quan_ly_tai_khoan.fchangname.textBox1_TextChanged(System.Object,System.EventArgs)")]
