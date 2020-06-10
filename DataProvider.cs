a901b6af377c82aa2e9ea5156b49e8c73058ce8e
Le Hoang Khanh <lhkhanh1710@gmail.com>
2020-06-10 21:41:59 +0700

    DataProvider.cs

---
+++ DataProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quan_ly_tai_khoan
+{
+    public class DataProvider
+    {
+        public void login(string id, string pass)
+        {
+            fTableManager f = new fTableManager();
+            if (id == "admin" && pass == "1234")
+                f.ShowDialog();
+            else
+               MessageBox.Show("Mật khẩu hoặc tài khoản không đúng.Mời nhập lại");
+        }
+        public void updatename(string oldname, string newname, string password, string password1)
+        {
+            fTableManager f = new fTableManager();
+            if (oldname == "admin" && newname == "" && password == "1234" && password1 == "1234")
+                f.ShowDialog();
+            else
+                MessageBox.Show("Chưa nhập đúng thông tin. Yêu cầu nhập lại!");
+        }
+        public void updatepassword(string oldpass, string newpass, string newpass2)
+        {
+            fTableManager f = new fTableManager();
+            if (oldpass == "1234" && newpass == "" && newpass2 == "" )
+                f.ShowDialog();
+            else
+                MessageBox.Show("Chưa nhập đúng thông tin. Yêu cầu nhập lại!");
+        }
+    }
+}
