3049d83580599670ec3f4e8217f799526f5dfaab
Le Hoang Khanh <lhkhanh1710@gmail.com>
2020-06-17 00:10:36 +0700

    Update fTableManager.Designer.cs

--- fTableManager.Designer.cs
+++ fTableManager.Designer.cs
@@ -199,21 +199,21 @@
             // đổiTênTàiKhoảnToolStripMenuItem
             // 
             this.đổiTênTàiKhoảnToolStripMenuItem.Name = "đổiTênTàiKhoảnToolStripMenuItem";
-            this.đổiTênTàiKhoảnToolStripMenuItem.Size = new System.Drawing.Size(210, 26);
+            this.đổiTênTàiKhoảnToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.đổiTênTàiKhoảnToolStripMenuItem.Text = "Đổi Tên Tài khoản";
             this.đổiTênTàiKhoảnToolStripMenuItem.Click += new System.EventHandler(this.đổiTênTàiKhoảnToolStripMenuItem_Click);
             // 
             // đổiMậtKhẩuToolStripMenuItem
             // 
             this.đổiMậtKhẩuToolStripMenuItem.Name = "đổiMậtKhẩuToolStripMenuItem";
-            this.đổiMậtKhẩuToolStripMenuItem.Size = new System.Drawing.Size(210, 26);
+            this.đổiMậtKhẩuToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.đổiMậtKhẩuToolStripMenuItem.Text = "Đổi Mật Khẩu";
             this.đổiMậtKhẩuToolStripMenuItem.Click += new System.EventHandler(this.đổiMậtKhẩuToolStripMenuItem_Click);
             // 
             // đổiMậtKhẩuPINToolStripMenuItem
             // 
             this.đổiMậtKhẩuPINToolStripMenuItem.Name = "đổiMậtKhẩuPINToolStripMenuItem";
-            this.đổiMậtKhẩuPINToolStripMenuItem.Size = new System.Drawing.Size(210, 26);
+            this.đổiMậtKhẩuPINToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.đổiMậtKhẩuPINToolStripMenuItem.Text = "Đổi Mật Khẩu PIN";
             this.đổiMậtKhẩuPINToolStripMenuItem.Click += new System.EventHandler(this.đổiMậtKhẩuPINToolStripMenuItem_Click);
             // 
