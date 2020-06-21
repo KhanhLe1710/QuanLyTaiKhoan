47056af91eeceb4fe2ccec5d9c92f6e7182a1d4b
Le Hoang Khanh <lhkhanh1710@gmail.com>
2020-06-21 22:49:56 +0700

    Create PINTest1.cs

---
+++ PINTEST/PINTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quan_ly_tai_khoan;
+
+namespace PINTEST
+{
+    [TestClass]
+    public class PINTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            fChangePIN obj = new fChangePIN();
+            bool mapinhientai = obj.doimapin(1234, 1235);
+            Assert.IsTrue(mapinhientai);
+        }
+    }
+}
