97aab91525b15fb232145a57c458b2791da32bd3
Le Hoang Khanh <lhkhanh1710@gmail.com>
2020-06-21 18:41:43 +0700

    Update fChangePIN.cs

--- fChangePIN.cs
+++ fChangePIN.cs
@@ -19,33 +19,49 @@ namespace Quan_ly_tai_khoan
         private void txtoldPIN_TextChanged(object sender, EventArgs e)
         {
             txtoldPIN.MaxLength = 4;
-            long n;
-            long.TryParse(txtoldPIN.Text, out n);
+            bool n;
+            
         }
         private void txtnewPIN_TextChanged_1(object sender, EventArgs e)
         {
             txtnewPIN.MaxLength = 4;
-            long n;
-            long.TryParse(txtnewPIN.Text, out n);
+            bool n;
+            
         }
         private void txtnewPIN2_TextChanged(object sender, EventArgs e)
         {
             txtnewPIN2.MaxLength = 4;
-            long n;
-            long.TryParse(txtnewPIN2.Text, out n);
+            bool n;
+            
         }
+        static bool doimapin(int mapinhientai, int newPIN)
+        {
+            if (newPIN != mapinhientai)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
         private void btupdate_Click(object sender, EventArgs e)
         {
             if (txtnewPIN.TextLength != 4 || txtnewPIN2.TextLength != 4 || txtoldPIN.TextLength != 4 && txtoldPIN.Text != "1234")
-                MessageBox.Show("Chưa đúng yêu cầu. Vui lòng nhập lại.", "Thông báo",MessageBoxButtons.RetryCancel);
+                MessageBox.Show("Chưa đúng yêu cầu. Vui lòng nhập lại.", "Thông báo", MessageBoxButtons.RetryCancel);
             else
             {
                 if (txtnewPIN2.Text != txtnewPIN.Text)
                     MessageBox.Show("Mật khẩu không trùng khớp");
                 else
                 {
-                    MessageBox.Show("Thay đổi mật khẩu PIN thành công");
-                    this.Close();
+                    bool ketqua;
+                    ketqua = doimapin(1234, 123);
+                    if (ketqua == true)
+                    {
+                        MessageBox.Show("Thay đổi mật khẩu PIN thành công");
+                    }
+                    else
+                        MessageBox.Show("Không thay đổi được mật khẩu PIN");
                 }
             }
         }
